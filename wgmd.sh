10c922f704188500e122a933b8fb768232b64f01
myh333777 <myh333777@gmail.com>
2021-09-15 12:26:29 +0800

    Update wgmd.sh

--- wgmd.sh
+++ wgmd.sh
@@ -163,8 +163,8 @@ function w64(){
 		apt-get update -y &&  apt install sudo -y
 		apt -y --no-install-recommends install openresolv dnsutils wireguard-tools
 	fi
-wget -N -6 https://cdn.jsdelivr.net/gh/kkkyg/WarpEUserv/wgcf -O /usr/local/bin/wgcf
-wget -N -6 https://cdn.jsdelivr.net/gh/kkkyg/WarpEUserv/wireguard-go -O /usr/bin/wireguard-go
+wget -N -6 https://cdn.jsdelivr.net/gh/myh333777/WarpEUserv/wgcf -O /usr/local/bin/wgcf
+wget -N -6 https://cdn.jsdelivr.net/gh/myh333777/WarpEUserv/wireguard-go -O /usr/bin/wireguard-go
 chmod +x /usr/local/bin/wgcf
 chmod +x /usr/bin/wireguard-go
 echo | wgcf register
@@ -200,8 +200,8 @@ function w646(){
 		apt-get update -y &&  apt install sudo -y
 		apt -y --no-install-recommends install openresolv dnsutils wireguard-tools
 	fi
-wget -N -6 https://cdn.jsdelivr.net/gh/kkkyg/WarpEUserv/wgcf -O /usr/local/bin/wgcf
-wget -N -6 https://cdn.jsdelivr.net/gh/kkkyg/WarpEUserv/wireguard-go -O /usr/bin/wireguard-go
+wget -N -6 https://cdn.jsdelivr.net/gh/myh333777/WarpEUserv/wgcf -O /usr/local/bin/wgcf
+wget -N -6 https://cdn.jsdelivr.net/gh/myh333777/WarpEUserv/wireguard-go -O /usr/bin/wireguard-go
 chmod +x /usr/local/bin/wgcf
 chmod +x /usr/bin/wireguard-go
 echo | wgcf register
@@ -238,8 +238,8 @@ function w66(){
 		apt-get update -y &&  apt install sudo -y
 		apt -y --no-install-recommends install openresolv dnsutils wireguard-tools
 	fi
-wget -N -6 https://cdn.jsdelivr.net/gh/kkkyg/WarpEUserv/wgcf -O /usr/local/bin/wgcf
-wget -N -6 https://cdn.jsdelivr.net/gh/kkkyg/WarpEUserv/wireguard-go -O /usr/bin/wireguard-go
+wget -N -6 https://cdn.jsdelivr.net/gh/myh333777/WarpEUserv/wgcf -O /usr/local/bin/wgcf
+wget -N -6 https://cdn.jsdelivr.net/gh/myh333777/WarpEUserv/wireguard-go -O /usr/bin/wireguard-go
 chmod +x /usr/local/bin/wgcf
 chmod +x /usr/bin/wireguard-go
 echo | wgcf register
@@ -297,18 +297,18 @@ systemctl status wg-quick@wgcf
 }
 
 function up4(){
-wget -N --no-check-certificate https://raw.githubusercontent.com/kkkyg/WarpEUserv/main/wgmd.sh && chmod +x wgmd.sh && ./wgmd.sh
+wget -N --no-check-certificate https://raw.githubusercontent.com/myh333777/WarpEUserv/main/wgmd.sh && chmod +x wgmd.sh && ./wgmd.sh
 }
 
 function up6(){
 echo -e nameserver 2a00:1098:2c::1 > /etc/resolv.conf
-wget -6 -N --no-check-certificate https://raw.githubusercontent.com/kkkyg/WarpEUserv/main/wgmd.sh && chmod +x wgmd.sh && ./wgmd.sh
+wget -6 -N --no-check-certificate https://raw.githubusercontent.com/myh333777/WarpEUserv/main/wgmd.sh && chmod +x wgmd.sh && ./wgmd.sh
 }
 
 #主菜单
 function start_menu(){
     clear
-    yellow " 详细说明 https://github.com/kkkyg/WarpEUserv  YouTube频道：甬哥侃侃侃" 
+    yellow " 详细说明 https://github.com/myh333777/WarpEUserv  YouTube频道：甬哥侃侃侃" 
     
     red " 切记：进入脚本快捷方式 bash wgmd.sh "
     
